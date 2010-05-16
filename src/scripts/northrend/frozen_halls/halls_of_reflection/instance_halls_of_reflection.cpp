44b03ee0a15b288ae439c2114defd8a5f038be25
Myh <mihai.dumitru@gxgnet.ro>
2010-05-17 00:43:33 +0300

    Sql Fixes
    Signed-off-by: Myh <mihai.dumitru@gxgnet.ro>

--- src/scripts/northrend/frozen_halls/halls_of_reflection/instance_halls_of_reflection.cpp
+++ src/scripts/northrend/frozen_halls/halls_of_reflection/instance_halls_of_reflection.cpp
@@ -16,6 +16,7 @@
 
 #include "ScriptedPch.h"
 #include "halls_of_reflection.h"
+#include "ScriptedEscortAI.h"
 
 #define MAX_ENCOUNTER 3
 
@@ -130,16 +131,15 @@ struct instance_halls_of_reflection : public ScriptedInstance
         OUT_LOAD_INST_DATA(in);
 
         char dataHead1, dataHead2;
-        uint16 data0, data1, data2;
+        uint16 data0, data1, data2, data3;
 
         std::istringstream loadStream(in);
-        loadStream >> dataHead1 >> dataHead2 >> data0 >> data1 >> data2;
+        loadStream >> dataHead1 >> dataHead2 >> data0 >> data1 >> data2 >> data3;
 
         if (dataHead1 == 'H' && dataHead2 == 'R')
         {
             m_auiEncounter[0] = data0;
             m_auiEncounter[1] = data1;
-            m_auiEncounter[2] = data2;
 
             for (uint8 i = 0; i < MAX_ENCOUNTER; ++i)
                 if (m_auiEncounter[i] == IN_PROGRESS)
@@ -151,6 +151,113 @@ struct instance_halls_of_reflection : public ScriptedInstance
     }
 };
 
+struct npc_slyvanas_windrunner_halls_of_reflection_partone : public ScriptedAI
+{
+    npc_slyvanas_windrunner_halls_of_reflection_partone(Creature *pCreature) : ScriptedAI(pCreature)
+    {
+        Reset();
+    }
+
+    uint64 uiLoralenGUID;
+    uint64 uiSlyvanasGUID;
+    uint64 uiLichKingGUID;
+    uint64 uiMargwynGUID;
+    uint64 uiFalricGUID;
+	uint64 uiUtherGUID;
+    uint64 uiMercGUID[ENCOUNTER_WAVE_MERCANERY];
+    uint64 uiFootmanGUID[ENCOUNTER_WAVE_FOOTMAN];
+    uint64 uiRifleGUID[ENCOUNTER_WAVE_RIFLEMAN];
+    uint64 uiPriestGUID[ENCOUNTER_WAVE_PRIEST];
+    uint64 uiMageGUID[ENCOUNTER_WAVE_MAGE];
+
+    bool eventstarted;
+    uint32 uiStep;
+    uint32 uiPhase_timer;
+
+void Reset()
+		{
+            eventstarted = false;
+            uiStep = 0;
+            uiPhase_timer = 3000;
+
+            me->SetStandState(UNIT_STAND_STATE_STAND);
+            me->SetVisibility(VISIBILITY_ON);
+            uiLoralenGUID = NULL;
+            uiSlyvanasGUID = NULL;
+            uiLichKingGUID = NULL;
+            uiMargwynGUID = NULL;
+            uiFalricGUID = NULL;
+			uiUtherGUID = NULL;
+        }
+
+   void JumpToNextStep(uint32 uiTimer)
+    {
+        uiPhase_timer = uiTimer;
+        ++uiStep;
+    }
+
+    void UpdateAI(const uint32 diff)
+    {
+
+            if (uiPhase_timer <= diff)
+            {
+                switch (uiStep)
+                {
+                    case 0:
+                            me->RemoveUnitMovementFlag(MOVEMENTFLAG_WALK_MODE);
+                            me->SendMonsterMove(HallsofReflectionLocs[0].x, HallsofReflectionLocs[0].y, HallsofReflectionLocs[0].z, HallsofReflectionLocs[0].o, 3, 1 );
+							me->setDeathState(ALIVE);
+							DoScriptText(SAY_SLYVANAS_2, me);
+							JumpToNextStep(3000);
+						break;
+                    case 1:
+                        if (Unit* pTemp = me->SummonCreature(NPC_UTHER, HallsofReflectionLocs[3].x, HallsofReflectionLocs[3].y, HallsofReflectionLocs[3].z, HallsofReflectionLocs[3].o, TEMPSUMMON_MANUAL_DESPAWN))
+                        {
+                            DoScriptText(SAY_UTHER_1, me);
+                            pTemp->RemoveUnitMovementFlag(MOVEMENTFLAG_WALK_MODE);
+                            uiUtherGUID = pTemp->GetGUID();
+                        }
+                        break;
+				}
+
+        }
+    }
+};
+
+bool GossipHello_npc_slyvanas_windrunner_halls_of_reflection_partone(Player* pPlayer, Creature* pCreature)
+{
+    if (pCreature->isQuestGiver())
+        pPlayer->PrepareQuestMenu( pCreature->GetGUID() );
+
+        pPlayer->ADD_GOSSIP_ITEM( 0, "Can you remove the sword?", GOSSIP_SENDER_MAIN, GOSSIP_ACTION_INFO_DEF+1);
+	    pPlayer->ADD_GOSSIP_ITEM( 0, "Dark Lady, I think I hear Arthas coming. Whatever you're going to do, do it quickly.", GOSSIP_SENDER_MAIN, GOSSIP_ACTION_INFO_DEF+2);
+
+    pPlayer->SEND_GOSSIP_MENU(DEFAULT_GOSSIP_MESSAGE, pCreature->GetGUID());
+
+    return true;
+}
+
+bool GossipSelect_npc_slyvanas_windrunner_halls_of_reflection_partone(Player* pPlayer, Creature* pCreature, uint32 uiSender, uint32 uiAction)
+{
+    switch (uiAction)
+    {
+        case GOSSIP_ACTION_INFO_DEF+1:
+            pPlayer->CLOSE_GOSSIP_MENU();
+            CAST_AI(npc_slyvanas_windrunner_halls_of_reflection_partone, pCreature->AI())->uiStep = 1;
+            break;
+        case GOSSIP_ACTION_INFO_DEF+2:
+            pPlayer->CLOSE_GOSSIP_MENU();
+            CAST_AI(npc_slyvanas_windrunner_halls_of_reflection_partone, pCreature->AI())->uiStep = 1;
+            break;
+    }
+    return true;
+}
+
+CreatureAI* GetAI_npc_slyvanas_hallsofreflection_partoneAI(Creature* pCreature)
+{
+    return new npc_slyvanas_windrunner_halls_of_reflection_partone(pCreature);
+}
+
 InstanceData* GetInstanceData_instance_halls_of_reflection(Map* pMap)
 {
     return new instance_halls_of_reflection(pMap);
@@ -159,7 +266,14 @@ InstanceData* GetInstanceData_instance_halls_of_reflection(Map* pMap)
 void AddSC_halls_of_reflection()
 {
     Script *newscript;
+
     newscript = new Script;
+    newscript->Name = "npc_slyvanas_hor_start";
+    newscript->GetAI = &GetAI_npc_slyvanas_hallsofreflection_partoneAI;
+    newscript->pGossipHello =  &GossipHello_npc_slyvanas_windrunner_halls_of_reflection_partone;
+    newscript->pGossipSelect = &GossipSelect_npc_slyvanas_windrunner_halls_of_reflection_partone;
+    newscript->RegisterSelf();
+
     newscript->Name = "instance_halls_of_reflection";
     newscript->GetInstanceData = &GetInstanceData_instance_halls_of_reflection;
     newscript->RegisterSelf();
