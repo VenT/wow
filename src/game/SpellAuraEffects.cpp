ca554bb2e47794c2652422335498907363558b49
Myh <mihai.dumitru@gxgnet.ro>
2010-05-18 06:31:59 +0300

    Revert "Implement anticheat"
    This reverts commit 70ba590c3f968d7d46d9ead1eda681338a9f5fd7.

--- src/game/SpellAuraEffects.cpp
+++ src/game/SpellAuraEffects.cpp
@@ -3624,15 +3624,9 @@ void AuraEffect::HandleAuraAllowFlight(AuraApplication const * aurApp, uint8 mod
         // allow fly
         WorldPacket data;
         if (apply)
-		{
-			((Player*)target)->SetCanFly(true);
-			data.Initialize(SMSG_MOVE_SET_CAN_FLY, 12);
-		}
+            data.Initialize(SMSG_MOVE_SET_CAN_FLY, 12);
         else
-		{
-			 data.Initialize(SMSG_MOVE_UNSET_CAN_FLY, 12);
-			((Player*)target)->SetCanFly(false);
-		}
+            data.Initialize(SMSG_MOVE_UNSET_CAN_FLY, 12);
         data.append(target->GetPackGUID());
         data << uint32(0);                                      // unk
         plr->SendDirectMessage(&data);
@@ -4041,16 +4035,10 @@ void AuraEffect::HandleAuraModIncreaseFlightSpeed(AuraApplication const * aurApp
             {
                 WorldPacket data;
                 if (apply)
-				{
-					((Player*)target)->SetCanFly(true);
                     data.Initialize(SMSG_MOVE_SET_CAN_FLY, 12);
-				}
                 else
-				{
                     data.Initialize(SMSG_MOVE_UNSET_CAN_FLY, 12);
-					((Player*)target)->SetCanFly(false);
-				}
-				data.append(plr->GetPackGUID());
+                data.append(plr->GetPackGUID());
                 data << uint32(0);                                      // unknown
                 plr->SendDirectMessage(&data);
             }
