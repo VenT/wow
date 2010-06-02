a89d0f5e9f0ea110d5dc405c8ad8d213bee487ed
Myh <mihai.dumitru@gxgnet.ro>
2010-06-02 16:18:37 +0300

    Light of Dawn is working with CTDB
    Signed-off-by: Myh <mihai.dumitru@gxgnet.ro>

--- sql/wow_ro/19_quest_template.sql
+++ sql/wow_ro/19_quest_template.sql
@@ -1,8 +1,6 @@
 -- Temp fix for some quests
 
 -- Death Knight Starting Zone
--- The Light of Dawn
-UPDATE `quest_template` SET `method`='0' WHERE `entry`='12801';
 -- Into the Realm of Shadows
 UPDATE `quest_template` SET `method`='0' WHERE `entry`='12687';
 -- Behind Scarlet Lines
