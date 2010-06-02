6de7e7d3e5b707fc72c32abff15ed076752a07ce
Myh <mihai.dumitru@gxgnet.ro>
2010-06-02 12:18:26 +0300

    New sqls
    Signed-off-by: Myh <mihai.dumitru@gxgnet.ro>

---
+++ sql/wow_ro/19_quest_template.sql
@@ -0,0 +1,5 @@
+-- Temp fix for some quests
+-- The Light of Dawn
+UPDATE `quest_template` SET `method`='0' WHERE `entry`='12801';
+-- Into the Realm of Shadows
+UPDATE `quest_template` SET `method`='0' WHERE `entry`='12687';
