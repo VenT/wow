4a00b5e185c872ed364c126371b035cbfe9a1ca1
Myh <mihai.dumitru@gxgnet.ro>
2010-06-23 13:03:58 +0300

    Revert "Acherus portal"
    This reverts commit 9ccac2b4c74302c184d1ac9c18328f5d1d8148ab.

--- sql/wow_ro/25_acherus_teleporter.sql
+++
@@ -1,3 +0,0 @@
-UPDATE `creature_template` SET `AIName`= "EventAI" WHERE `entry` IN (29588,29589,29580,29581);
-DELETE FROM `creature_ai_scripts` WHERE `creature_id` IN (29588,29589,29580,29581);
-INSERT INTO `creature_ai_scripts` VALUES (2958801, 29588, 1, 0, 100, 1, 0, 0, 5000, 5000, 11, 54742, 0, 2, 0, 0, 0, 0, 0, 0, 0, 0, 'Teleport - Hall -> Heart (EPL)'), (2958901, 29589, 1, 0, 100, 1, 0, 0, 5000, 5000, 11, 54745, 0, 2, 0, 0, 0, 0, 0, 0, 0, 0, 'Teleport - Heart -> Hall (EPL)'), (2958001, 29580, 1, 0, 100, 1, 0, 0, 5000, 5000, 11, 54700, 0, 2, 0, 0, 0, 0, 0, 0, 0, 0, 'Teleport - Hall -> Heart'), (2958101, 29581, 1, 0, 100, 1, 0, 0, 5000, 5000, 11, 54724, 0, 2, 0, 0, 0, 0, 0, 0, 0, 0, 'Teleport - Heart -> Hall');
