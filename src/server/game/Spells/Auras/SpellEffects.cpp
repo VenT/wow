43a76730f1254db4018c81b25124305eb14923be
Myh <mihai.dumitru@gxgnet.ro>
2010-06-27 14:11:17 +0300

    Dispell code cleanup - GetDispelChance now really returns dispel chance and not resist chance.

--- src/server/game/Spells/Auras/SpellEffects.cpp
+++ src/server/game/Spells/Auras/SpellEffects.cpp
@@ -3913,7 +3913,7 @@ void Spell::EffectDispel(uint32 i)
 
         bool success = false;
         // 2.4.3 Patch Notes: "Dispel effects will no longer attempt to remove effects that have 100% dispel resistance."
-        if (GetDispelChance(itr->first->GetCaster(), unitTarget, itr->first->GetId(), !unitTarget->IsFriendlyTo(m_caster), &success) > 99)
+        if (!GetDispelChance(itr->first->GetCaster(), unitTarget, itr->first->GetId(), !unitTarget->IsFriendlyTo(m_caster), &success))
         {
             dispel_list.erase(itr);
             continue;
@@ -7258,7 +7258,7 @@ void Spell::EffectDispelMechanic(uint32 i)
             continue;
         bool success = false;
         GetDispelChance(aura->GetCaster(), unitTarget, aura->GetId(), !unitTarget->IsFriendlyTo(m_caster), &success);
-        if((GetAllSpellMechanicMask(aura->GetSpellProto()) & (1<<(mechanic))) && success)
+        if ((GetAllSpellMechanicMask(aura->GetSpellProto()) & (1 << mechanic)) && success)
             dispel_list.push(std::make_pair(aura->GetId(), aura->GetCasterGUID()));
     }
 
@@ -7649,7 +7649,7 @@ void Spell::EffectStealBeneficialBuff(uint32 i)
 
         bool success = false;
         // 2.4.3 Patch Notes: "Dispel effects will no longer attempt to remove effects that have 100% dispel resistance."
-        if (GetDispelChance(itr->first->GetCaster(), unitTarget, itr->first->GetId(), !unitTarget->IsFriendlyTo(m_caster), &success) > 99)
+        if (!GetDispelChance(itr->first->GetCaster(), unitTarget, itr->first->GetId(), !unitTarget->IsFriendlyTo(m_caster), &success))
         {
             steal_list.erase(itr);
             continue;
