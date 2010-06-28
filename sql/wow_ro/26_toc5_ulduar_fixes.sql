1b5f3842a91e3c9d9c2183e0e0cfb049655e4aef
Myh <mihai.dumitru@gxgnet.ro>
2010-06-28 15:58:41 +0300

    Fix Trial of the Champion. Some fixes for Ulduar.

---
+++ sql/wow_ro/26_toc5_ulduar_fixes.sql
@@ -0,0 +1,120 @@
+INSERT IGNORE INTO `creature_ai_scripts` VALUES ('3291551', '32915', '0', '0', '100', '7', '8000', '10000', '10000', '16000', '11', '62239', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', 'Brightleaf Flux');
+INSERT IGNORE INTO `creature_ai_scripts` VALUES ('3291551', '32915', '0', '0', '100', '7', '8000', '10000', '10000', '16000', '11', '62251', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', 'Brightleaf Fluxv2');
+INSERT IGNORE INTO `creature_ai_scripts` VALUES ('3291552', '32915', '0', '0', '100', '25', '8000', '10000', '10000', '16000', '11', '62251', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', 'Brightleaf H');
+INSERT IGNORE INTO `creature_ai_scripts` VALUES ('3291552', '32915', '0', '0', '100', '25', '12000', '15000', '16000', '18000', '11', '62239', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', 'Brightleaf H');
+INSERT IGNORE INTO `creature_ai_scripts` VALUES ('3291553', '32915', '0', '0', '100', '7', '10000', '14000', '30000', '50000', '11', '62240', '4', '0', '0', '0', '0', '0', '0', '0', '0', '0', 'Solar Flare');
+INSERT IGNORE INTO `creature_ai_scripts` VALUES ('3291554', '32915', '0', '0', '100', '25', '10000', '14000', '30000', '50000', '11', '62920', '4', '0', '0', '0', '0', '0', '0', '0', '0', '0', 'Solar Flare H');
+INSERT IGNORE INTO `creature_ai_scripts` VALUES ('3291555', '32915', '0', '0', '100', '7', '20000', '25000', '20000', '25000', '11', '62211', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', 'Unstable Sun');
+INSERT IGNORE INTO `creature_ai_scripts` VALUES ('3291556', '32915', '0', '0', '100', '25', '20000', '25000', '20000', '25000', '11', '62211', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', 'Unstable Sun H');
+INSERT IGNORE INTO `creature_ai_scripts` VALUES ('3291555', '32915', '0', '0', '100', '7', '15000', '20000', '13000', '19000', '11', '62217', '1', '0', '0', '0', '0', '0', '0', '0', '0', '0', 'Unstable Energy');
+INSERT IGNORE INTO `creature_ai_scripts` VALUES ('3291556', '32915', '0', '0', '100', '25', '15000', '25000', '13000', '19000', '11', '62922', '1', '0', '0', '0', '0', '0', '0', '0', '0', '0', 'Unstable Energy H');
+INSERT IGNORE INTO `creature_ai_scripts` VALUES ('3291555', '32915', '0', '0', '100', '7', '20000', '25000', '20000', '25000', '11', '62209', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', 'Heal');
+INSERT IGNORE INTO `creature_ai_scripts` VALUES ('3291555', '32915', '0', '0', '100', '25', '20000', '25000', '20000', '25000', '11', '62209', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', 'Heal');
+UPDATE `creature_template` SET `AIName` = 'EventAI' WHERE `entry` = '32915';
+
+INSERT IGNORE INTO `creature_ai_scripts` VALUES ('3291355', '32913', '0', '0', '100', '7', '20000', '25000', '20000', '25000', '11', '62310', '1', '0', '0', '0', '0', '0', '0', '0', '0', '0', 'Impale');
+INSERT IGNORE INTO `creature_ai_scripts` VALUES ('3291356', '32913', '0', '0', '100', '25', '20000', '25000', '20000', '25000', '11', '62928', '1', '0', '0', '0', '0', '0', '0', '0', '0', '0', 'Impale H');
+INSERT IGNORE INTO `creature_ai_scripts` VALUES ('3291355', '32913', '0', '0', '100', '7', '15000', '20000', '13000', '19000', '11', '62438', '1', '0', '0', '0', '0', '0', '0', '0', '0', '0', 'Iron Roots');
+INSERT IGNORE INTO `creature_ai_scripts` VALUES ('3291356', '32913', '0', '0', '100', '25', '15000', '25000', '13000', '19000', '11', '62930', '1', '0', '0', '0', '0', '0', '0', '0', '0', '0', 'Iron Roots H');
+INSERT IGNORE INTO `creature_ai_scripts` VALUES ('3291355', '32913', '0', '0', '100', '7', '20000', '25000', '20000', '25000', '11', '62285', '1', '0', '0', '0', '0', '0', '0', '0', '0', '0', 'Thorn');
+INSERT IGNORE INTO `creature_ai_scripts` VALUES ('3291355', '32913', '0', '0', '100', '25', '20000', '25000', '20000', '25000', '11', '63552', '1', '0', '0', '0', '0', '0', '0', '0', '0', '0', 'Thorn H');
+UPDATE `creature_template` SET `AIName` = 'EventAI' WHERE `entry` = '32913';
+
+INSERT IGNORE INTO `creature_ai_scripts` VALUES ('3291455', '32914', '0', '0', '100', '7', '20000', '25000', '30000', '35000', '11', '62344', '1', '0', '0', '0', '0', '0', '0', '0', '0', '0', 'Fists of Stone');
+INSERT IGNORE INTO `creature_ai_scripts` VALUES ('3291456', '32914', '0', '0', '100', '25', '20000', '25000', '30000', '35000', '11', '62344', '1', '0', '0', '0', '0', '0', '0', '0', '0', '0', 'Fists of Stone H');
+INSERT IGNORE INTO `creature_ai_scripts` VALUES ('3291455', '32914', '0', '0', '100', '7', '15000', '20000', '13000', '19000', '11', '62325', '4', '0', '0', '0', '0', '0', '0', '0', '0', '0', 'Ground Tremor');
+INSERT IGNORE INTO `creature_ai_scripts` VALUES ('3291456', '32914', '0', '0', '100', '25', '15000', '25000', '13000', '19000', '11', '62932', '4', '0', '0', '0', '0', '0', '0', '0', '0', '0', 'Ground Tremor H');
+INSERT IGNORE INTO `creature_ai_scripts` VALUES ('3291455', '32914', '0', '0', '100', '7', '20000', '25000', '20000', '25000', '11', '62337', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', 'Petrified Bark');
+INSERT IGNORE INTO `creature_ai_scripts` VALUES ('3291455', '32914', '0', '0', '100', '25', '20000', '25000', '20000', '25000', '11', '62933', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', 'Petrified Bark H');
+UPDATE `creature_template` SET `AIName` = 'EventAI' WHERE `entry` = '32914';
+
+UPDATE `creature_template` SET `faction_A` = '16', `faction_H` = '16' WHERE `entry` = '35451';
+UPDATE `creature_template` SET `faction_A` = '16', `faction_H` = '16' WHERE `entry` = '12012';
+UPDATE `creature_template` SET `faction_A` = '35', `faction_H` = '35' WHERE `entry` = '35491';
+UPDATE `creature_template` SET `faction_A` = '16', `faction_H` = '16', `health_mod` = '4', `spell1` = '68301', `spell2` = '68504', `AIName` = 'EventAI', `dmg_multiplier` = '26' WHERE `entry` = '35633';
+UPDATE `creature_template` SET `faction_A` = '16', `faction_H` = '16', `health_mod` = '4', `spell1` = '68301', `spell2` = '68504', `AIName` = 'EventAI', `dmg_multiplier` = '26' WHERE `entry` = '35768';
+UPDATE `creature_template` SET `faction_A` = '16', `faction_H` = '16', `health_mod` = '4', `spell1` = '68301', `spell2` = '68504', `AIName` = 'EventAI', `dmg_multiplier` = '26' WHERE `entry` = '34658';
+UPDATE `creature_template` SET `faction_A` = '16', `faction_H` = '16', `health_mod` = '4', `spell1` = '68301', `spell2` = '68504', `AIName` = 'EventAI', `dmg_multiplier` = '26' WHERE `entry` = '35636';
+UPDATE `creature_template` SET `faction_A` = '16', `faction_H` = '16', `health_mod` = '4', `spell1` = '68301', `spell2` = '68504', `AIName` = 'EventAI' WHERE `entry` = '35638';
+UPDATE `creature_template` SET `faction_A` = '16', `faction_H` = '16', `health_mod` = '4', `spell1` = '68301', `spell2` = '68504', `AIName` = 'EventAI', `dmg_multiplier` = '26' WHERE `entry` = '35635';
+UPDATE `creature_template` SET `faction_A` = '16', `faction_H` = '16', `health_mod` = '4', `spell1` = '68301', `spell2` = '68504', `AIName` = 'EventAI', `dmg_multiplier` = '26' WHERE `entry` = '35640';
+UPDATE `creature_template` SET `faction_A` = '16', `faction_H` = '16', `health_mod` = '4', `spell1` = '68301', `spell2` = '68504', `AIName` = 'EventAI', `dmg_multiplier` = '26' WHERE `entry` = '35641';
+UPDATE `creature_template` SET `faction_A` = '16', `faction_H` = '16', `health_mod` = '4', `spell1` = '68301', `spell2` = '68504', `AIName` = 'EventAI', `dmg_multiplier` = '26' WHERE `entry` = '35634';
+UPDATE `creature_template` SET `faction_A` = '16', `faction_H` = '16', `health_mod` = '4', `spell1` = '68301', `spell2` = '68504', `AIName` = 'EventAI', `dmg_multiplier` = '26' WHERE `entry` = '35637';
+
+UPDATE `creature_template` SET `faction_A` = '16', `faction_H` = '16', `minlevel` = '80', `maxlevel` = '80', `spell1` = '68301', `spell2` = '68504', `AIName` = 'EventAI', `health_mod` = '4' WHERE `entry` = '33297';
+UPDATE `creature_template` SET `faction_A` = '16', `faction_H` = '16', `minlevel` = '80', `maxlevel` = '80', `spell1` = '68301', `spell2` = '68504', `AIName` = 'EventAI', `health_mod` = '4' WHERE `entry` = '33408';
+UPDATE `creature_template` SET `faction_A` = '16', `faction_H` = '16', `minlevel` = '80', `maxlevel` = '80', `spell1` = '68301', `spell2` = '68504', `AIName` = 'EventAI', `health_mod` = '4' WHERE `entry` = '33298';
+UPDATE `creature_template` SET `faction_A` = '16', `faction_H` = '16', `minlevel` = '80', `maxlevel` = '80', `spell1` = '68301', `spell2` = '68504', `AIName` = 'EventAI', `health_mod` = '4' WHERE `entry` = '33301';
+UPDATE `creature_template` SET `faction_A` = '16', `faction_H` = '16', `minlevel` = '80', `maxlevel` = '80', `spell1` = '68301', `spell2` = '68504', `AIName` = 'EventAI', `health_mod` = '4' WHERE `entry` = '33416';
+UPDATE `creature_template` SET `faction_A` = '16', `faction_H` = '16', `minlevel` = '80', `maxlevel` = '80', `spell1` = '68301', `spell2` = '68504', `AIName` = 'EventAI', `health_mod` = '4' WHERE `entry` = '33300';
+UPDATE `creature_template` SET `faction_A` = '16', `faction_H` = '16', `minlevel` = '80', `maxlevel` = '80', `spell1` = '68301', `spell2` = '68504', `AIName` = 'EventAI', `health_mod` = '4' WHERE `entry` = '33418';
+UPDATE `creature_template` SET `faction_A` = '16', `faction_H` = '16', `minlevel` = '80', `maxlevel` = '80', `spell1` = '68301', `spell2` = '68504', `AIName` = 'EventAI', `health_mod` = '4' WHERE `entry` = '33409';
+UPDATE `creature_template` SET `faction_A` = '16', `faction_H` = '16', `minlevel` = '80', `maxlevel` = '80', `spell1` = '68301', `spell2` = '68504', `AIName` = 'EventAI', `health_mod` = '4' WHERE `entry` = '33414';
+UPDATE `creature_template` SET `faction_A` = '16', `faction_H` = '16', `minlevel` = '80', `maxlevel` = '80', `spell1` = '68301', `spell2` = '68504', `AIName` = 'EventAI', `health_mod` = '4' WHERE `entry` = '33299';
+
+INSERT IGNORE INTO `creature_ai_scripts` VALUES ('3291501', '32915', '0', '0', '100', '7', '2000', '2500', '2000', '2500', '11', '68301', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', 'Charge');
+INSERT IGNORE INTO `creature_ai_scripts` VALUES ('3291502', '32915', '0', '0', '100', '25', '2000', '2500', '2000', '2500', '11', '68301', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', 'Charge');
+INSERT IGNORE INTO `creature_ai_scripts` VALUES ('3291503', '32915', '0', '0', '100', '7', '2000', '2500', '2000', '2500', '11', '68504', '1', '0', '0', '0', '0', '0', '0', '0', '0', '0', 'Break');
+INSERT IGNORE INTO `creature_ai_scripts` VALUES ('3291504', '32915', '0', '0', '100', '25', '5000', '5500', '5000', '5500', '11', '68504', '1', '0', '0', '0', '0', '0', '0', '0', '0', '0', 'Break');
+
+INSERT IGNORE INTO `creature_ai_scripts` VALUES ('3563301', '35633', '0', '0', '100', '7', '2000', '2500', '2000', '2500', '11', '68301', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', 'Charge');
+INSERT IGNORE INTO `creature_ai_scripts` VALUES ('3563302', '35633', '0', '0', '100', '25', '2000', '2500', '2000', '2500', '11', '68301', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', 'Charge');
+INSERT IGNORE INTO `creature_ai_scripts` VALUES ('3563303', '35633', '0', '0', '100', '7', '2000', '2500', '2000', '2500', '11', '68504', '1', '0', '0', '0', '0', '0', '0', '0', '0', '0', 'Break');
+INSERT IGNORE INTO `creature_ai_scripts` VALUES ('3563304', '35633', '0', '0', '100', '25', '5000', '5500', '5000', '5500', '11', '68504', '1', '0', '0', '0', '0', '0', '0', '0', '0', '0', 'Break');
+
+INSERT IGNORE INTO `creature_ai_scripts` VALUES ('3576801', '35768', '0', '0', '100', '7', '2000', '2500', '2000', '2500', '11', '68301', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', 'Charge');
+INSERT IGNORE INTO `creature_ai_scripts` VALUES ('3576802', '35768', '0', '0', '100', '25', '2000', '2500', '2000', '2500', '11', '68301', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', 'Charge');
+INSERT IGNORE INTO `creature_ai_scripts` VALUES ('3576803', '35768', '0', '0', '100', '7', '2000', '2500', '2000', '2500', '11', '68504', '1', '0', '0', '0', '0', '0', '0', '0', '0', '0', 'Break');
+INSERT IGNORE INTO `creature_ai_scripts` VALUES ('3576804', '35768', '0', '0', '100', '25', '5000', '5500', '5000', '5500', '11', '68504', '1', '0', '0', '0', '0', '0', '0', '0', '0', '0', 'Break');
+
+INSERT IGNORE INTO `creature_ai_scripts` VALUES ('3465801', '34658', '0', '0', '100', '7', '2000', '2500', '2000', '2500', '11', '68301', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', 'Charge');
+INSERT IGNORE INTO `creature_ai_scripts` VALUES ('3465802', '34658', '0', '0', '100', '25', '2000', '2500', '2000', '2500', '11', '68301', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', 'Charge');
+INSERT IGNORE INTO `creature_ai_scripts` VALUES ('3465803', '34658', '0', '0', '100', '7', '2000', '2500', '2000', '2500', '11', '68504', '1', '0', '0', '0', '0', '0', '0', '0', '0', '0', 'Break');
+INSERT IGNORE INTO `creature_ai_scripts` VALUES ('3465804', '34658', '0', '0', '100', '25', '5000', '5500', '5000', '5500', '11', '68504', '1', '0', '0', '0', '0', '0', '0', '0', '0', '0', 'Break');
+
+INSERT IGNORE INTO `creature_ai_scripts` VALUES ('3563601', '35636', '0', '0', '100', '7', '2000', '2500', '2000', '2500', '11', '68301', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', 'Charge');
+INSERT IGNORE INTO `creature_ai_scripts` VALUES ('3563602', '35636', '0', '0', '100', '25', '2000', '2500', '2000', '2500', '11', '68301', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', 'Charge');
+INSERT IGNORE INTO `creature_ai_scripts` VALUES ('3563603', '35636', '0', '0', '100', '7', '2000', '2500', '2000', '2500', '11', '68504', '1', '0', '0', '0', '0', '0', '0', '0', '0', '0', 'Break');
+INSERT IGNORE INTO `creature_ai_scripts` VALUES ('3563604', '35636', '0', '0', '100', '25', '5000', '5500', '5000', '5500', '11', '68504', '1', '0', '0', '0', '0', '0', '0', '0', '0', '0', 'Break');
+
+INSERT IGNORE INTO `creature_ai_scripts` VALUES ('3563801', '35638', '0', '0', '100', '7', '2000', '2500', '2000', '2500', '11', '68301', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', 'Charge');
+INSERT IGNORE INTO `creature_ai_scripts` VALUES ('3563802', '35638', '0', '0', '100', '25', '2000', '2500', '2000', '2500', '11', '68301', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', 'Charge');
+INSERT IGNORE INTO `creature_ai_scripts` VALUES ('3563803', '35638', '0', '0', '100', '7', '2000', '2500', '2000', '2500', '11', '68504', '1', '0', '0', '0', '0', '0', '0', '0', '0', '0', 'Break');
+INSERT IGNORE INTO `creature_ai_scripts` VALUES ('3563804', '35638', '0', '0', '100', '25', '5000', '5500', '5000', '5500', '11', '68504', '1', '0', '0', '0', '0', '0', '0', '0', '0', '0', 'Break');
+
+INSERT IGNORE INTO `creature_ai_scripts` VALUES ('3563501', '35635', '0', '0', '100', '7', '2000', '2500', '2000', '2500', '11', '68301', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', 'Charge');
+INSERT IGNORE INTO `creature_ai_scripts` VALUES ('3563502', '35635', '0', '0', '100', '25', '2000', '2500', '2000', '2500', '11', '68301', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', 'Charge');
+INSERT IGNORE INTO `creature_ai_scripts` VALUES ('3563503', '35635', '0', '0', '100', '7', '2000', '2500', '2000', '2500', '11', '68504', '1', '0', '0', '0', '0', '0', '0', '0', '0', '0', 'Break');
+INSERT IGNORE INTO `creature_ai_scripts` VALUES ('3563504', '35635', '0', '0', '100', '25', '5000', '5500', '5000', '5500', '11', '68504', '1', '0', '0', '0', '0', '0', '0', '0', '0', '0', 'Break');
+
+INSERT IGNORE INTO `creature_ai_scripts` VALUES ('3564101', '35641', '0', '0', '100', '7', '2000', '2500', '2000', '2500', '11', '68301', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', 'Charge');
+INSERT IGNORE INTO `creature_ai_scripts` VALUES ('3564102', '35641', '0', '0', '100', '25', '2000', '2500', '2000', '2500', '11', '68301', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', 'Charge');
+INSERT IGNORE INTO `creature_ai_scripts` VALUES ('3564103', '35641', '0', '0', '100', '7', '2000', '2500', '2000', '2500', '11', '68504', '1', '0', '0', '0', '0', '0', '0', '0', '0', '0', 'Break');
+INSERT IGNORE INTO `creature_ai_scripts` VALUES ('3564104', '35641', '0', '0', '100', '25', '5000', '5500', '5000', '5500', '11', '68504', '1', '0', '0', '0', '0', '0', '0', '0', '0', '0', 'Break');
+
+INSERT IGNORE INTO `creature_ai_scripts` VALUES ('3563401', '35634', '0', '0', '100', '7', '2000', '2500', '2000', '2500', '11', '68301', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', 'Charge');
+INSERT IGNORE INTO `creature_ai_scripts` VALUES ('3563402', '35634', '0', '0', '100', '25', '2000', '2500', '2000', '2500', '11', '68301', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', 'Charge');
+INSERT IGNORE INTO `creature_ai_scripts` VALUES ('3563403', '35634', '0', '0', '100', '7', '2000', '2500', '2000', '2500', '11', '68504', '1', '0', '0', '0', '0', '0', '0', '0', '0', '0', 'Break');
+INSERT IGNORE INTO `creature_ai_scripts` VALUES ('3563404', '35634', '0', '0', '100', '25', '5000', '5500', '5000', '5500', '11', '68504', '1', '0', '0', '0', '0', '0', '0', '0', '0', '0', 'Break');
+
+INSERT IGNORE INTO `creature_ai_scripts` VALUES ('3564001', '35640', '0', '0', '100', '7', '2000', '2500', '2000', '2500', '11', '68301', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', 'Charge');
+INSERT IGNORE INTO `creature_ai_scripts` VALUES ('3564002', '35640', '0', '0', '100', '25', '2000', '2500', '2000', '2500', '11', '68301', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', 'Charge');
+INSERT IGNORE INTO `creature_ai_scripts` VALUES ('3564003', '35640', '0', '0', '100', '7', '2000', '2500', '2000', '2500', '11', '68504', '1', '0', '0', '0', '0', '0', '0', '0', '0', '0', 'Break');
+INSERT IGNORE INTO `creature_ai_scripts` VALUES ('3564004', '35640', '0', '0', '100', '25', '5000', '5500', '5000', '5500', '11', '68504', '1', '0', '0', '0', '0', '0', '0', '0', '0', '0', 'Break');
+
+UPDATE `creature_template` SET `dmg_multiplier` = '12' WHERE `entry` = '14809';
+UPDATE `creature_template` SET `dmg_multiplier` = '14' WHERE `entry` = '14808';
+UPDATE `creature_template` SET `dmg_multiplier` = '26' WHERE `entry` = '14810';
+
+
+
+
+
+
+
+
+
+
+
+
