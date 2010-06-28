376db9ab5ced0ea08985532b8b035266df96c4eb
Myh <mihai.dumitru@gxgnet.ro>
2010-06-29 02:37:09 +0300

    Not good.

--- src/server/game/OutdoorPvP/Zones/OutdoorPvPWG.cpp
+++ src/server/game/OutdoorPvP/Zones/OutdoorPvPWG.cpp
@@ -1559,7 +1559,7 @@ void OutdoorPvPWG::EndBattle()
         {
             Creature *veh = *m_vehicles[team].begin();
             m_vehicles[team].erase(m_vehicles[team].begin());
-            vehicle->RemoveAllPassengers();
+            //((Vehicle*)veh)->RemoveAllPassengers();
             veh->ForcedDespawn();
         }
 
