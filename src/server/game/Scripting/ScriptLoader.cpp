d0315ddaee3591cabc5b10878e0dc3258c197b0b
Myh <mihai.dumitru@gxgnet.ro>
2010-06-30 19:52:32 +0300

    LOL: Forgot loading Eye of Eternity scripts, my bad.

--- src/server/game/Scripting/ScriptLoader.cpp
+++ src/server/game/Scripting/ScriptLoader.cpp
@@ -367,6 +367,8 @@ void AddSC_boss_drakos();                //The Nexus The Oculus
 void AddSC_boss_urom();
 void AddSC_instance_oculus();
 void AddSC_oculus();
+void AddSC_boss_malygos();                //The Nexus: Eye of Eternity
+void AddSC_instance_eye_of_eternity();
 void AddSC_boss_sartharion();            //Obsidian Sanctum
 void AddSC_instance_obsidian_sanctum();
 void AddSC_boss_bjarngrim();             //Ulduar Halls of Lightning
@@ -892,6 +894,8 @@ void AddScripts()
     AddSC_boss_urom();
     AddSC_instance_oculus();
     AddSC_oculus();
+    AddSC_boss_malygos();                //The Nexus: Eye of Eternity
+    AddSC_instance_eye_of_eternity();
     AddSC_boss_sartharion();            //Obsidian Sanctum
     AddSC_instance_obsidian_sanctum();
     AddSC_boss_bjarngrim();             //Ulduar Halls of Lightning
