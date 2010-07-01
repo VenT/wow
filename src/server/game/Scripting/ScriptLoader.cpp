a38b4fdca1ea9077e0f1438d5a5dcdab95dcc05f
Myh <mihai.dumitru@gxgnet.ro>
2010-07-01 18:48:16 +0300

    Adjust includes for some Ulduar-scripts (fixes non-PCH build on *nix and should also do the same for Visual Studio/Windows).

--- src/server/game/Scripting/ScriptLoader.cpp
+++ src/server/game/Scripting/ScriptLoader.cpp
@@ -387,6 +387,7 @@ void AddSC_boss_assembly_of_iron();
 void AddSC_boss_hodir();
 void AddSC_boss_freya();
 void AddSC_boss_thorim();
+void AddSC_boss_mimiron();
 void AddSC_ulduar_teleporter();
 void AddSC_instance_ulduar();
 void AddSC_boss_keleseth();              //Utgarde Keep
@@ -917,6 +918,7 @@ void AddScripts()
     AddSC_boss_hodir();
     AddSC_boss_freya();
     AddSC_boss_thorim();
+    AddSC_boss_mimiron();
     AddSC_ulduar_teleporter();
     AddSC_instance_ulduar();
     AddSC_boss_keleseth();              //Utgarde Keep
