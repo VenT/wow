b3121b9548701174c6872d70f0ad021be342ceb7
Myh <mihai.dumitru@gxgnet.ro>
2010-07-06 23:53:53 +0300

    Wrong one.

--- sql/scripts/world_scripts_full.sql
+++ sql/scripts/world_scripts_full.sql
@@ -1002,20 +1002,11 @@ UPDATE `gameobject_template` SET `ScriptName`='containment_sphere' WHERE `entry`
 
 
 /* OBSIDIAN SANCTUM */
-UPDATE `instance_template` SET `script`='instance_obsidian_sanctum' WHERE `map`=615;
-UPDATE `creature_template` SET `ScriptName`='boss_sartharion' WHERE `entry`=28860;
-UPDATE `creature_template` SET `ScriptName`='mob_vesperon' WHERE `entry`=30449;
-UPDATE `creature_template` SET `ScriptName`='mob_shadron' WHERE `entry`=30451;
-UPDATE `creature_template` SET `ScriptName`='mob_tenebron' WHERE `entry`=30452;
-UPDATE `creature_template` SET `ScriptName`='mob_twilight_eggs' WHERE `entry`=30882;
-UPDATE `creature_template` SET `ScriptName`='mob_twilight_whelp' WHERE `entry` IN (30890,31214);
-UPDATE `creature_template` SET `ScriptName`='npc_twilight_fissure' WHERE `entry`=30641;
-UPDATE `creature_template` SET `ScriptName`='mob_acolyte_of_shadron' WHERE `entry`=31218;
-UPDATE `creature_template` SET `ScriptName`='mob_acolyte_of_vesperon' WHERE `entry`=31219;
-UPDATE `creature_template` SET `ScriptName`='npc_flame_tsunami' WHERE `entry`=30616;
+
 
 /* ONYXIA'S LAIR */
-
+UPDATE `instance_template` SET `script`='instance_onyxias_lair' WHERE `map`=249;	
+UPDATE `creature_template` SET `ScriptName`='boss_onyxia' WHERE `entry`=10184;
 
 /* ORGRIMMAR */
 UPDATE `creature_template` SET `ScriptName`='npc_neeru_fireblade' WHERE `entry`=3216;
