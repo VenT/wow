9980543dd83368992a1bb9584b82d06eb5dc9d9c
Myh <mihai.dumitru@gxgnet.ro>
2010-07-06 18:36:53 +0300

    Revert "Fix some deprecated checks on MapManager::CanPlayerEnter()."
    This reverts commit 2b793a395e2303545c3cc33fcada2e2b968cc297.

--- src/server/game/Maps/MapManager.cpp
+++ src/server/game/Maps/MapManager.cpp
@@ -228,13 +228,13 @@ bool MapManager::CanPlayerEnter(uint32 mapid, Player* player, bool loginCheck)
     //Get instance where player's group is bound & its map
     if (pGroup)
     {
-        InstanceGroupBind* boundedInstance = pGroup->GetBoundInstance(entry);
+        InstanceGroupBind* boundedInstance = pGroup->GetBoundInstance(player);
         if (boundedInstance && boundedInstance->save)
         {
             if (Map *boundedMap = sMapMgr.FindMap(mapid,boundedInstance->save->GetInstanceId()))
             {
                 // Player permanently bounded to different instance than groups one
-                InstancePlayerBind* playerBoundedInstance = player->GetBoundInstance(mapid, player->GetDifficulty(entry->IsRaid()));
+                InstancePlayerBind* playerBoundedInstance = player->GetBoundInstance(mapid, player->GetDungeonDifficulty());
                 if (playerBoundedInstance && playerBoundedInstance->perm && playerBoundedInstance->save &&
                     boundedInstance->save->GetInstanceId() != playerBoundedInstance->save->GetInstanceId())
                 {
